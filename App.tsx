3d1b0f13c64683743c5db8f8b85670c080b2a5fe
Dagostini01 <pedro01dagostini@gmail.com>
2025-05-14 17:58:18 -0300

    feat: estrutura da aplicacao

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
-import { Text, View } from 'react-native';
+import { ActivityIndicator, StatusBar, Text, View } from 'react-native';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto"
 
 export default function App() {
+
+  const [fontLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home</Text>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      {fontLoaded ? <Text>Home</Text> : <ActivityIndicator />}
     </View>
   );
 }
